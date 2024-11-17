2a0ec82d3dcbeebf662c832fe743f03a10119adf
LiorLin <liorlin12@gmail.com>
2024-11-17 19:27:26 +0200

    Adding files

---
+++ day02/circle.py
@@ -0,0 +1,10 @@
+# This is a file for assignment 2
+
+radius = 3
+pi = 3.14
+
+area = pi*(radius**2)
+circumference = 2*pi*radius
+
+print (f"The area of the circle is {area}.")
+print (f"The circumference of the circle is {circumference}.")
